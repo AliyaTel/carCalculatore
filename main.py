1bb5c845e42c949c0999abeebf3d4985818536ec
AliyaTel <teltaevaalia@gmail.com>
2024-04-02 16:24:23 +0600

    change something

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ if __name__ == '__main__':
                       service_cost=200, insurance_cost=2500)
     )
     calc.add_car(
-        calculate.ElectricCar("Tesla model 3", 200000, 5500, 150)
+        calculate.ElectricCar("Tesla model 3", 200000, 5500, 15)
     )
     calc.add_car(
-        calculate.Car("Range Rover", 650000, 3,
+        calculate.Car("Range Rover", 650000, 4,
+                      service_cost=3000, insurance_cost=7000)
+    )
+    calc.add_car(
+        calculate.Car("Bentley", 650000, 4,
                       service_cost=3000, insurance_cost=7000)
     )
     calc.print_cars()
